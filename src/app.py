d086d998b38705df161babd3231ef732a47e1506
He Ma <114445202+hema2022ubc@users.noreply.github.com>
2024-04-06 14:09:26 -0700

    Add the genre dropdown and top two plots (#31)
    * add the artist-time chart and explicit chart together
    
    * Modify the opacity
    
    * fixed after merging
    
    ---------
    
    Co-authored-by: Rachel Bouwer <45149769+rbouwer@users.noreply.github.com>
    Co-authored-by: RACHEL BOUWER <r.12.bouwer@gmail.com>

--- src/app.py
+++ src/app.py
@@ -2,6 +2,9 @@ from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import dash_bootstrap_components as dbc
 import ast
+import dash_vega_components as dvc
+import altair as alt
+from itertools import product
 
 # Initiatlize the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -16,7 +19,7 @@ def convert_string_to_list(string):
     # Return an empty list if the string is not a valid list representation
     except ValueError:
         return []  
-        
+
 tracks_df['genres'] = tracks_df['genres'].apply(convert_string_to_list)
 unique_genres = sorted(set(genre for sublist in tracks_df['genres'] for genre in sublist))
 genre_dropdown_options = [{'label': genre, 'value': genre} for genre in unique_genres]
@@ -24,7 +27,7 @@ genre_dropdown_options = [{'label': genre, 'value': genre} for genre in unique_g
 # Configuration
 genre_dropdown = dcc.Dropdown(
     options= genre_dropdown_options,
-    multi=True,
+    multi=False,
     placeholder='Select a genre...',
     id='genre-dropdown'
 )
@@ -50,7 +53,11 @@ year_range_selector = dbc.Row([
             id='end-year'
         ))
 ])
-summary_statistics = html.Div([
+
+artist_time_chart =  dvc.Vega(id='artist-time-chart', spec={})
+explicit_chart = dvc.Vega(id='explicit-chart', spec={})
+
+summary_statistics = dbc.Col([
     html.Br(),
     html.H4('Song features (Mean)', className='text-center'),
     html.H5('Top 5 Popular Songs', className='text-center'),
@@ -84,6 +91,8 @@ summary_statistics = html.Div([
 app.layout = dbc.Container([
     dbc.Row([
         html.Title('Spotify Popularity Dashboard'),
+        html.Label('Select the genre you want to analyze:'),
+        genre_dropdown,
         html.Label('Select the artists you want to analyze:'),
         html.Br(),
         artist_dropdown,
@@ -94,8 +103,12 @@ app.layout = dbc.Container([
     ]),
     dbc.Row([
         dbc.Col(
-            html.Label('Plots will go here')
+            # [html.Label('Plots will go here'),
+           dbc.Row( [dbc.Col(html.Div(artist_time_chart)),
+            dbc.Col(html.Div(explicit_chart))
+            ])
         ),
+
         dbc.Col(
             summary_statistics, width=3
         )
@@ -181,9 +194,86 @@ def update_artist_dropdown(selected_genres):
     artist_options = [{'label': artist, 'value': artist} for artist in filtered_artists['artist'].unique()]
     return artist_options
 
+@app.callback(
+    Output('artist-time-chart', 'spec'),
+    [
+        Input('artists-dropdown', 'value'),
+        Input('start-year', 'value'),
+        Input('end-year', 'value')
+    ]
+)
+
+def update_time_chart(selected_artists, start_year, end_year):
+    if selected_artists is None or start_year is None or end_year is None:
+        return {}
+    tracks_df_filtered = tracks_df[(tracks_df['artist'].isin(selected_artists)) &
+                                       (tracks_df['release_year'] >= start_year) &
+                                       (tracks_df['release_year'] <= end_year)]
+    chart = alt.Chart(tracks_df_filtered).mark_point(opacity=0.7).encode(
+        x=alt.X('release_year', 
+                scale=alt.Scale(domain=[start_year, end_year]),
+                axis=alt.Axis(format=''),
+                title='Release Year'),
+        y=alt.Y('mean(popularity)', title='Popularity'),
+        color=alt.Color('artist', legend=alt.Legend(title="Artist")),
+        tooltip=['artist', 'release_year', 'mean(popularity)']
+    ).properties(
+        title='Artist Popularity Over Time'  # Add a title to the chart
+    )
+    chart = chart + chart.mark_line()
+    return chart.to_dict()
+
+
+
+@app.callback(
+    Output('explicit-chart', 'spec'),
+    [
+        Input('artists-dropdown', 'value'),
+        Input('start-year', 'value'),
+        Input('end-year', 'value')
+    ]
+)
+
+def create_explicit_chart(selected_artists, start_year, end_year):
+    if selected_artists is None or start_year is None or end_year is None:
+        return {}
+    
+    all_combinations = pd.DataFrame(list(product(selected_artists, ['Clean', 'Explicit'])),
+                                    columns=['artist', 'song_type'])
+    
+    all_combinations['popularity'] = 0
+
+
+    df_filtered = tracks_df[(tracks_df['artist'].isin(selected_artists)) &
+                                       (tracks_df['release_year'] >= start_year) &
+                                       (tracks_df['release_year'] <= end_year)].copy()
+    df_filtered['song_type'] = tracks_df['explicit'].map({1: 'Explicit', 0: 'Clean'})
+    grouped = df_filtered.groupby(['artist', 'song_type'])['popularity'].mean().reset_index()
+    
+    
+    # Merge the all_combinations dataframe with the grouped data to fill in actual popularity values
+    merged_df = pd.merge(all_combinations, grouped, on=['artist', 'song_type'], how='left', suffixes=('', '_actual'))
+    merged_df['popularity'] = merged_df['popularity_actual'].fillna(0)
+    merged_df.drop(columns='popularity_actual', inplace=True)
+    merged_df['artist'] = merged_df['artist'].apply(lambda x: x.split()[0] if ' ' in x else x)
+
+
+    chart =  alt.Chart(merged_df).mark_bar().encode(
+        alt.X('song_type:N', axis=alt.Axis(title=None, labels=True, ticks=True)),
+        alt.Y('popularity:Q', axis=alt.Axis(title='Mean Popularity', grid=False)),
+        color=alt.Color('song_type:N', legend=alt.Legend(title="Song Type")),
+        column=alt.Column('artist:N', header=alt.Header(title=None, labelOrient='bottom'))
+    ).configure_view(
+        stroke='transparent'
+    ).properties(width = 20,
+        title='Mean Popularity of Songs by Type and Artist'
+    )
+
+    return chart.to_dict()
+
 # Run the app/dashboard
 if __name__ == '__main__':
     import dash
 
     print("dash version=", dash.__version__)
-    app.run(debug=False)
+    app.run(debug=True)
