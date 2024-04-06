e39a313a4200208ccbc9adb05dbf57795fa6be13
RACHEL BOUWER <r.12.bouwer@gmail.com>
2024-04-06 13:59:14 -0700

    added bar plot for top songs

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import dash_bootstrap_components as dbc
+import dash_vega_components as dvc
+import altair as alt
 
 # Initiatlize the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -69,6 +71,8 @@ bottom_blub=[
     html.P("Last deployed on April 6, 2023",
            style={"font-size": "12px"})
            ]
+top5songs_barchart = dvc.Vega(id='top5songs-barchart', spec={})
+
 
 # Layout
 app.layout = dbc.Container([
@@ -83,9 +87,13 @@ app.layout = dbc.Container([
         html.Br()
     ]),
     dbc.Row([
-        dbc.Col(
-            html.Label('Plots will go here')
-        ),
+        dbc.Col([
+            dbc.Row(html.Label('He Ma\'s plots to go here')),
+            dbc.Row([
+                dbc.Col(top5songs_barchart),
+                dbc.Col(html.Label('Another plot to go here'))
+            ])
+        ]),
         dbc.Col(
             summary_statistics, width=3
         )
@@ -159,6 +167,32 @@ def display_artist_tracks(selected_artists, start_year, end_year):
         
         return card_mean_danceability, card_mean_energy, card_mean_loudness, card_mean_speechiness, card_mean_acousticness, \
                card_mean_instrumentalness, card_mean_liveness, card_mean_valence
+    
+@app.callback(
+    Output("top5songs-barchart", 'spec'),
+    [
+        Input('artists-dropdown', 'value'),
+        Input('start-year', 'value'),
+        Input('end-year', 'value')
+    ]
+)
+def update_top_songs_bar_chart(selected_artists, start_year, end_year):
+    if selected_artists is None or start_year is None or end_year is None:
+        return {}
+    tracks_df_filtered = tracks_df[(tracks_df['artist'].isin(selected_artists)) &
+                                       (tracks_df['release_year'] >= start_year) &
+                                       (tracks_df['release_year'] <= end_year)]
+    tracks_df_filtered_top_five = tracks_df_filtered.sort_values('popularity', ascending=False).iloc[:5]
+    fig = alt.Chart(tracks_df_filtered_top_five, width='container').mark_bar().encode(
+        x=alt.X('popularity', title="Popularity"),
+        y=alt.Y('name', title="Song Name").sort('-x'),
+        color=alt.Color('artist', legend=None).scale(scheme="greens"),
+        tooltip=['artist','release_year']
+    ).properties(
+        title='Popularity of Top Songs'
+    ).to_dict()
+    
+    return fig
 
 
 # Run the app/dashboard
